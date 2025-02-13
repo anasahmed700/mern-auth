4d6c7650d98548c25bbb206d64a468d26684c9ed
Anas Ahmed <anasahmed700@gmail.com>
2025-02-13 17:02:27 +0500

    create get user data route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import 'dotenv/config';
 
 import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,5 +17,6 @@ app.use(cors({credentials: true}));
 
 app.get('/', (req, res) => res.send("API works! Hello, world!"));
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
